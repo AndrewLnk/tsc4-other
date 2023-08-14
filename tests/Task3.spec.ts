21242d8dd57481d3cbdedeb08d22c3d25d351d41
AndrewLnk <andrew.olenk@gmail.com>
2023-08-14 07:38:24 +0300

    added cache logic

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -76,14 +76,15 @@ describe('Task3', () => {
     it('1 -> 10', async () => {
 
         const b1 = new Builder();
+        b1.storeBit(0);
         b1.storeBit(1);
         b1.storeBit(1);
         const data = b1.endCell();
 
-        const result = await task3.getResult(1n, 65846858677786564796789677789789789789n, data);
+        const result = await task3.getResult(3n, 65846858677786564796789677789789789789n, data);
         var resultLine = await task3.getBitsLine(result);
         
-        var dataLine = await task3.replaceCellAll(data, "1", "110001100010011010010011001000101011100010100000100010111110011110111001101000011000100010111101100000110111111001111001011101");
+        var dataLine = await task3.replaceCellAll(data, "11", "110001100010011010010011001000101011100010100000100010111110011110111001101000011000100010111101100000110111111001111001011101");
         
         expect(resultLine).toBe(dataLine);
     });
@@ -108,8 +109,19 @@ describe('Task3', () => {
 
         var resultLine = await task3.getBitsLine(result);
         var dataLine = await task3.getBitsLine(data3);
-        dataLine = dataLine.replace("1100", "1111");
-        expect(resultLine).toBe(dataLine);
+        var dataLine_rep = dataLine.replace("1100", "1111");
+        
+        if (resultLine == dataLine_rep)
+        {
+            expect(resultLine).toBe(dataLine_rep);
+        }
+        else
+        {
+            let w = dataLine;
+            w += "=>";
+            w += resultLine;
+            expect(w).toBe(dataLine_rep);
+        }
     });
 
     it('8, 9, replace join', async () => {
@@ -235,28 +247,46 @@ describe('Task3', () => {
 
     it('10', async () => 
     {
-        for (var r = 0; r < 0; r++)
+        for (var r = 0; r < 100; r++)
         {
             var b = new Builder();
-            b.storeInt(123, 8);
+            b.storeBit(0);
             var data = b.endCell();
 
-            for (var i = 0; i < 5; i++)
+            for (var i = 0; i < randomInt(1, 127); i++)
             {
                 b = new Builder();
-                b.storeBuffer(randomBytes(randomInt(1, 127)));
+                b.storeBuffer(randomBytes(randomInt(1, 30)));
                 b.storeRef(data);
                 data = b.endCell();
             }
+            
+            var f = randomInt(1, 1024);
+            var v = randomInt(1, 1024);
 
-            const result = await task3.getResult(1n, 2n, data);
+            const result = await task3.getResult(BigInt(f), BigInt(v), data);
             var resultLine = await task3.getBitsLine(result);
 
-            var rep = await task3.replaceCellAll(data, "1", "10");
+            function dec2bin(v:number) {
+                return (v >>> 0).toString(2);
+            }
+
+            var rep = await task3.replaceCellAll(data, dec2bin(f), dec2bin(v));
+            var rep_first = await task3.getBitsLine(data);
 
             if (resultLine != rep)
             {
-                expect(resultLine).toBe(rep);
+                var rt = resultLine;
+                rt += "   ";
+                rt += f;
+                rt += "=";
+                rt += dec2bin(f);
+                rt += "   ";
+                rt += v;
+                rt += "=";
+                rt += dec2bin(v);
+                
+                expect(rt).toBe(rep_first);
             }
         }
 
